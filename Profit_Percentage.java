1c754aeb6bf1fe8b9cc0da2994c7083865916bc5
koonisettimahesh <147791718+koonisettimahesh@users.noreply.github.com>
2024-07-31 15:32:04 +0530

    import java.util.Scanner; public class Frame{     public static void main(String[] args)     {         int a,b;         Scanner read=new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         double c=((b-a)*100.0)/a;         System.out.printf("%.2f",c);     } }

---
+++ Profit_Percentage.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Frame{
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner read=new Scanner(System.in);
+        a=read.nextInt();
+        b=read.nextInt();
+        double c=((b-a)*100.0)/a;
+        System.out.printf("%.2f",c);
+    }
+}
